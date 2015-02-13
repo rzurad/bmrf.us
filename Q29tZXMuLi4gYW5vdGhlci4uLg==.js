7dd783a0cde9f689d63545eda5720ffb834b591b
Richard Zurad <rzurad@gmail.com>
2015-02-12 23:53:25 -0800

    people are big on birthdays, lets start there

--- Q29tZXMuLi4gYW5vdGhlci4uLg==.js
+++ Q29tZXMuLi4gYW5vdGhlci4uLg==.js
@@ -32,14 +32,10 @@ $.hit = function (e, coords) {
         gain = context.createGain(),
         fuzz = new Audio('./MP/RGllLi4ueW91IGFsbCBkaWUuLi55b3UgYWxsIGRpZS4uLg=='),
         dot = 52,
-        times = {
-            '.': dot,
-            '-': dot * 3,
-            ' ': dot * 3,
-            '/': dot * 7
-        },
+        chars = [[' ', '.'], ['/', '-']],
+        times = { '.': dot, '-': dot * 3, ' ': dot * 3, '/': dot * 7 },
 
-        code = '...---.../...---.../...---.../-.. ./.- .. ..... -... --/.- .. ..... -... --/.- .. ..... -... --/-... .-.. .- -.-. -.-/-- . ... .-/.-. . ... . .- .-. -.-. ..../..-. .- -.-. .. .-.. .. - -.--/-... .. --- -.. --- -- ./.-.. .- -... ... .-.-.-/- .-. .- .--. .--. . -.. --..--/-. . . -../.-. . ... -.-. ..- . .-.-.-/. ...- .- -.-./--.. --- -. ./-. --- -/... .- ..-. . .-.-.-/-- .. .-.. .. - .- .-. -.--/.... --- ... - .. .-.. ./.-.-./-.-//////////////////////////////',
+        code = '64ab95eca3a4c9d56841ec8f55c641f4a2a16924c08ca862584605736908c0aca8e296ac07f1e70ec28aecc05c882d537ecc085e624b096bc05af17d260ca42c51e96282ee60c0ad0c762767c2868a1acf82490585c0ce085e62c62505ab719e6624e295d2b4c29d50f72a3eb6624d2b4ac04a80ebc5e71508b60c926b9e0efbc0c0b44055e8593da2e2e07ca38942828ec0ba562ceb242af498d1a24a4f0cb0f65a39de809b70fa0a39e2bfd091c8e0a7288da08067adad39d26a0cf6e048072a92ab40bc773e44425b7248c0d2ec2c74c2e1e5af435eb131133131333333333333331313313',
 
         img = new Image();
 
@@ -61,7 +57,10 @@ $.hit = function (e, coords) {
         }
 
         var oscillator = context.createOscillator(),
-            c = code.charAt(i),
+            num = parseInt(code.charAt(i), 16),
+            even = num % 2,
+            one = +(num >= 4),
+            c = chars[even][one],
             isStarted = false,
             time = times[c];
 
---
+++ convert.js
@@ -0,0 +1,39 @@
+(function () {
+    "use strict";
+
+    var code = '...---.../...---.../...---.../-.. ./.- .. ..... -... --/.- .. ..... -... --/.- .. ..... -... --/-... .-.. .- -.-. -.-/-- . ... .-/.-. . ... . .- .-. -.-. ..../..-. .- -.-. .. .-.. .. - -.--/-... .. --- -.. --- -- ./.-.. .- -... ... .-.-.-/- .-. .- .--. .--. . -.. --..--/-. . . -../.-. . ... -.-. ..- . .-.-.-/. ...- .- -.-./--.. --- -. ./-. --- -/... .- ..-. . .-.-.-/-- .. .-.. .. - .- .-. -.--/.... --- ... - .. .-.. ./.-.-./-.-//////////////////////////////',
+        converted = [],
+        decoded = [];
+
+    function randomFrom(a) {
+        return a[~~(Math.random() * a.length)];
+    }
+
+    for (var i = 0; i < code.length; i++) {
+        switch (code.charAt(i)) {
+            case '.': converted.push(randomFrom(['4', '6', '8', 'a', 'c', 'e'])); break;
+            case '-': converted.push(randomFrom(['5', '7', '9', 'b', 'd', 'f'])); break;
+            case ' ': converted.push(randomFrom(['0', '2'])); break;
+            case '/': converted.push(randomFrom(['1', '3'])); break;
+            default:
+                console.error('what the fuck, bro?');
+        }
+    }
+
+    converted = converted.join('');
+    console.log(converted);
+    
+    var chars = [[' ', '.'], ['/', '-']];
+
+    for (i = 0; i < converted.length; i++) {
+        var num = parseInt(converted.charAt(i), 16),
+            even = num % 2,
+            one = +(num >= 4);
+
+        decoded.push(chars[even][one]);
+    }
+
+    decoded = decoded.join('');
+
+    console.log(decoded === code);
+}.call(this));
